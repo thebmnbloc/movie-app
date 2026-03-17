ca03466b5b92b1ad6cbc05a522faf6865c87dd6c
thebmnbloc <bioralak46@gmail.com>
2026-03-17 11:12:14 +0200

    change app name

--- apps/server/src/routes/watchlistRoute.ts
+++ apps/server/src/routes/watchlistRoute.ts
@@ -10,12 +10,11 @@ const watchlistController = new WatchlistController();
 
 router.post('/', watchlistController.createWatchlist.bind(watchlistController));
 router.get('/:id', watchlistController.getWatchlistById.bind(watchlistController));
-router.get('/user/:userId', watchlistController.getUserWatchlists.bind(watchlistController));
-router.get('/public', watchlistController.getPublicWatchlists.bind(watchlistController));
-router.get('/movie/:movieId', watchlistController.getWatchlistsContainingMovie.bind(watchlistController));
 router.delete('/:id', watchlistController.deleteWatchlist.bind(watchlistController));
 router.put('/:id/movie/:movieId/notes', watchlistController.updateMovieNotes.bind(watchlistController));
 router.put('/:id', watchlistController.updateWatchlist.bind(watchlistController));
 router.post('/:id/movie', watchlistController.addMovieToWatchlist.bind(watchlistController));
+router.delete('/:id/movie/:movieId', watchlistController.removeMovieFromWatchlist.bind(watchlistController));
+router.get('/:id/movie/:movieId', watchlistController.isMovieInWatchlist.bind(watchlistController));
 
 export default router;
